a92b9bf68fb77155b631c05c554a590f7834f6d3
paulorjuniorp <paulorjuniorp@gmail.com>
2023-08-03 07:24:12 -0300

    Criação de Resource para User
    Adição de um controller que tem acesso ao User

---
+++ src/main/java/com/paulorjuniorp/webservices/springboot/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.paulorjuniorp.webservices.springboot.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.paulorjuniorp.webservices.springboot.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User user = new User(1, "Teste", "teste@gmail.com", "40028922", "1234");
+		return ResponseEntity.ok().body(user);
+	}
+}
