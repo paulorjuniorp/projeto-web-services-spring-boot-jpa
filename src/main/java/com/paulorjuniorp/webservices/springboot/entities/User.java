1b8eb3f0f11833c415c2f7211bcd076a6a996e3e
paulorjuniorp <paulorjuniorp@gmail.com>
2023-08-04 06:13:38 -0300

    Configuração do banco de dados
    Configuração do banco de dados h2 e jpa

--- src/main/java/com/paulorjuniorp/webservices/springboot/entities/User.java
+++ src/main/java/com/paulorjuniorp/webservices/springboot/entities/User.java
@@ -4,9 +4,7 @@ import java.io.Serializable;
 import java.util.Objects;
 
 public class User implements Serializable{
-	/**
-	 * 
-	 */
+	
 	private static final long serialVersionUID = 1L;
 	private Integer id;
 	private String name;
