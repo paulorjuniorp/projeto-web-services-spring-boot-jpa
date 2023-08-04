e5c6d5a7899a2e08f95f135aac3a0242201152a9
paulorjuniorp <paulorjuniorp@gmail.com>
2023-08-04 07:09:11 -0300

    Adição do método findById
    Adição do método findById dentro do resource e service

--- src/main/java/com/paulorjuniorp/webservices/springboot/resources/UserResource.java
+++ src/main/java/com/paulorjuniorp/webservices/springboot/resources/UserResource.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.paulorjuniorp.webservices.springboot.entities.User;
@@ -23,5 +25,12 @@ public class UserResource {
 		List<User> users = userService.findAll();
 		
 		return ResponseEntity.ok().body(users);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Integer id){
+		User user = userService.findById(id);
+		
+		return ResponseEntity.ok().body(user);
 	}
 }
--- src/main/java/com/paulorjuniorp/webservices/springboot/services/UserService.java
+++ src/main/java/com/paulorjuniorp/webservices/springboot/services/UserService.java
@@ -1,6 +1,7 @@
 package com.paulorjuniorp.webservices.springboot.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,5 +16,9 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return userRepository.findAll();
+	}
+	
+	public User findById(Integer id) {
+		return userRepository.findById(id).orElse(null);
 	}
 }
