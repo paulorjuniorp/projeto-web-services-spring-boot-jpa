2ceacbe110bc12cb10eefb7d021451b4ca073a85
paulorjuniorp <paulorjuniorp@gmail.com>
2023-08-07 06:57:47 -0300

    Criação da estrutura da classe de produtos
    Criação da classe Product

---
+++ src/main/java/com/paulorjuniorp/webservices/springboot/entities/Product.java
@@ -0,0 +1,90 @@
+package com.paulorjuniorp.webservices.springboot.entities;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Product implements Serializable {
+
+	
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String name;
+	private String description;
+	private Double price;
+	private String imgUrl;
+	
+	private List<Category> categories = new ArrayList<>();
+	private List<Order> orders = new ArrayList<>();
+	
+	public Product() {
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+
+	public List<Category> getCategories() {
+		return categories;
+	}
+
+	public List<Order> getOrders() {
+		return orders;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, name);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Product other = (Product) obj;
+		return Objects.equals(id, other.id) && Objects.equals(name, other.name);
+	}
+	
+}
