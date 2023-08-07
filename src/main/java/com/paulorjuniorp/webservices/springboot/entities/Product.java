e3d7dcfeddf39e35837fadee30a76256c58e52fc
paulorjuniorp <paulorjuniorp@gmail.com>
2023-08-07 07:11:36 -0300

    Adição das anotações do JPA em Product
    Adição do mapeamento JPA na classe de produto

--- src/main/java/com/paulorjuniorp/webservices/springboot/entities/Product.java
+++ src/main/java/com/paulorjuniorp/webservices/springboot/entities/Product.java
@@ -7,11 +7,21 @@ import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_product")
 public class Product implements Serializable {
 
 	
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String description;
