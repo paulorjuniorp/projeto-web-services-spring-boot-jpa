7634e090ddaaf140a1c03a7ce79212d2d7bdb9dc
paulorjuniorp <paulorjuniorp@gmail.com>
2023-08-18 07:15:55 -0300

    Adição do tratamento de id não encontrado durante o delete
    Adição do tratamento de id não encontrado durante o delete

--- src/main/java/com/paulorjuniorp/webservices/springboot/services/UserService.java
+++ src/main/java/com/paulorjuniorp/webservices/springboot/services/UserService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.paulorjuniorp.webservices.springboot.entities.User;
@@ -29,9 +30,10 @@ public class UserService {
 	}
 	
 	public void delete(Integer id) {
-		User user = this.findById(id);
-		if (user != null) {
-			userRepository.delete(user);
+		try {
+			userRepository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
 		}
 	}
 	
