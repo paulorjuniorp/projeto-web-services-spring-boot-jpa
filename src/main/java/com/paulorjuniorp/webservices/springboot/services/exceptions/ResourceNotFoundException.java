aa42b2a08afad53e839271cd0b8b927a1a2f0407
paulorjuniorp <paulorjuniorp@gmail.com>
2023-08-17 22:19:28 -0300

    Criação de uma classe ResourceNotFoundException para o Service
    Criação da classe ResourceNotFoundException para no caso de ser passado um objeto que não existe em uma consulta.

---
+++ src/main/java/com/paulorjuniorp/webservices/springboot/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.paulorjuniorp.webservices.springboot.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+	
+	private static final long serialVersionUID = 1L;
+
+}
