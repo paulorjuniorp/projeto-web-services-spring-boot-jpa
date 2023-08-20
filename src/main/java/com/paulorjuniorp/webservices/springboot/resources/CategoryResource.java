c1b011a5e59f12c970c1b76cc2b74a3cc127499b
paulorjuniorp <paulorjuniorp@gmail.com>
2023-08-20 12:07:00 -0300

    Adição dos métodos restantes no controller de categoria
    Adição dos métodos que faltavam em CategoryResource

--- src/main/java/com/paulorjuniorp/webservices/springboot/resources/CategoryResource.java
+++ src/main/java/com/paulorjuniorp/webservices/springboot/resources/CategoryResource.java
@@ -1,13 +1,19 @@
 package com.paulorjuniorp.webservices.springboot.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.paulorjuniorp.webservices.springboot.entities.Category;
 import com.paulorjuniorp.webservices.springboot.services.CategoryService;
@@ -31,5 +37,26 @@ public class CategoryResource {
 		Category user = categoryService.findById(id);
 		
 		return ResponseEntity.ok().body(user);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Category> insert(@RequestBody Category obj){
+		Category category = categoryService.insert(obj);
+		URI uri = ServletUriComponentsBuilder
+				.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(category.getId()).toUri();
+		return ResponseEntity.created(uri).body(category);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		categoryService.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Category> update(@PathVariable Long id, @RequestBody Category obj){
+		obj = categoryService.update(id, obj);
+		return ResponseEntity.ok().body(obj);
 	}
 }
