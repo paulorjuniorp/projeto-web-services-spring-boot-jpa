a6449c4ce1a090d3a57536efbabe22e3358049e4
paulorjuniorp <paulorjuniorp@gmail.com>
2023-08-21 06:45:16 -0300

    Adição das rotas de insert, update e delete no controller de produto
    Adição dos métodos que faltavam em ProductResource

--- src/main/java/com/paulorjuniorp/webservices/springboot/resources/ProductResource.java
+++ src/main/java/com/paulorjuniorp/webservices/springboot/resources/ProductResource.java
@@ -1,13 +1,19 @@
 package com.paulorjuniorp.webservices.springboot.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.paulorjuniorp.webservices.springboot.entities.Product;
 import com.paulorjuniorp.webservices.springboot.services.ProductService;
@@ -31,5 +37,26 @@ public class ProductResource {
 		Product user = productService.findById(id);
 		
 		return ResponseEntity.ok().body(user);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Product> insert(@RequestBody Product obj){
+		Product product = productService.insert(obj);
+		URI uri = ServletUriComponentsBuilder
+				.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(product);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		productService.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Product> update(@PathVariable Long id, @RequestBody Product obj){
+		obj = productService.update(id, obj);
+		return ResponseEntity.ok().body(obj);
 	}
 }
